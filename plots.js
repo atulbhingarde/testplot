cea2c06c4fb082eb17795404c8ef6c881f0da2a2
atulbhingarde <atul.bhingarde@ge.com>
2020-06-20 12:42:48 -0400

    update

--- plots.js
+++ plots.js
@@ -12,18 +12,29 @@ var apiKey = "YndWi3WEDL-sXiAVZSWT";
  * index 4 - Close
  * index 5 - Volume
  */
- 
+ var StartDate="2016-10-01";
+ var EndDate="2017-10-01";
+ var Symbol="AAPL";
+
  var inputField1 = d3.select("#datepicker1");
  var inputField2 = d3.select("#datepicker2");
+ var inputField3 = d3.select("#symbol");
+ Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
 function unpack(rows, index) {
   return rows.map(function(row) {
     return row[index];
   });
 }
 var Button = d3.select("#filter-btn");
-function getMonthlyData() {
+function getMonthlyData(Symbol, StartDate, EndDate) {
 
-  var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2016-10-01&end_date=2017-10-01&collapse=monthly&api_key=${apiKey}`;
+  var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&collapse=monthly&api_key=${apiKey}`;
+  console.log(queryUrl);
+
   d3.json(queryUrl).then(function(data) {
     var dates = unpack(data.dataset.data, 0);
     var openPrices = unpack(data.dataset.data, 1);
@@ -50,9 +61,9 @@ function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, vol
   }
 }
 
-function buildPlot() {
-  var url = `https://www.quandl.com/api/v3/datasets/WIKI/AMZN.json?start_date=2017-01-01&end_date=2018-11-22&api_key=${apiKey}`;
-
+function buildPlot(Symbol, StartDate, EndDate) {
+  var url = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&api_key=${apiKey}`;
+  console.log(url);
   d3.json(url).then(function(data) {
 
     // Grab values from the response json object to build the plots
@@ -66,7 +77,7 @@ function buildPlot() {
     var lowPrices = unpack(data.dataset.data, 3);
     var closingPrices = unpack(data.dataset.data, 4);
 
-    getMonthlyData();
+    getMonthlyData(Symbol, StartDate, EndDate);
 
     var trace1 = {
       type: "scatter",
@@ -109,14 +120,36 @@ function buildPlot() {
   });
 }
 Button.on("click", () => {
-  tbody.html("");
-  populate(data);
-  console.log("Table reset");
-  console.log(inputField1);
-  var inputField1 = $("#datepicker1").datepicker('getDate');
-  $.datepicker.formatDate('dd-mm-yy', inputField1);
-
+  
+  function formatdate(userDate){
+    var omar= new Date(userDate);
+    y  = omar.getFullYear().pad().toString();
+    m = omar.getMonth().pad(2).toString();
+    d = omar.getDate().pad(2).toString();
+    omar=y+"-"+m+"-"+d;
+    return omar;
+  }
+  // tbody.html("");
+  // populate(data);
+  // console.log("Table reset");
   // console.log(inputField1);
-  // console.log(inputField2);
+  var MySymbol=$("#symbol");
+  var inputField1 = $("#datepicker1").datepicker('getDate');
+  $.datepicker.formatDate('dd/mm/yy', inputField1);
+  var inputField2 = $("#datepicker2").datepicker('getDate');
+  $.datepicker.formatDate('dd/mm/yy', inputField2);
+  // D1=inputField1
+  var formatTime = d3.timeFormat("%Y-%m-%d");
+  var Symbol = inputField3.property("value").toString().toUpperCase().trim();
+  jj=Date.parse(inputField1);
+  // console.log((inputField1));
+  StartDate=formatTime(inputField1);
+  EndDate=formatTime(inputField2);
+  // console.log(inputCity);
+  // console.log(jj.getDate()+"-"+jj.getMonth()+"-"+jj.getFullYear());
+  // console.log(inputField1);
+  //console.log(inputField2);
+  // console.log(jj);
+  buildPlot(Symbol, StartDate, EndDate);
 });
 buildPlot();
