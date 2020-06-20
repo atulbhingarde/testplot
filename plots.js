6f29580d599aaa26fcc8acbd608560d3a5254e1f
atulbhingarde <atul.bhingarde@ge.com>
2020-06-20 16:35:25 -0400

    now one needs to key in the key on screen

--- plots.js
+++ plots.js
@@ -1,4 +1,4 @@
-var apiKey = "YndWi3WEDL-sXiAVZSWT";
+// var apiKey = "YndWi3WEDL-sXiAVZSWT";
 
 /**
  * Helper function to select stock data
@@ -19,6 +19,8 @@ var apiKey = "YndWi3WEDL-sXiAVZSWT";
  var inputField1 = d3.select("#datepicker1");
  var inputField2 = d3.select("#datepicker2");
  var inputField3 = d3.select("#symbol");
+ var mykeyAB = d3.select("#pass");
+ // console.log(mykeyA);
  Number.prototype.pad = function(size) {
   var s = String(this);
   while (s.length < (size || 2)) {s = "0" + s;}
@@ -30,10 +32,10 @@ function unpack(rows, index) {
   });
 }
 var Button = d3.select("#filter-btn");
-function getMonthlyData(Symbol, StartDate, EndDate) {
+function getMonthlyData(Symbol, StartDate, EndDate, mykeyA) {
 
-  var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&collapse=monthly&api_key=${apiKey}`;
-  console.log(queryUrl);
+  var queryUrl = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&collapse=monthly&api_key=${mykeyA}`;
+  // console.log(queryUrl);
 
   d3.json(queryUrl).then(function(data) {
     var dates = unpack(data.dataset.data, 0);
@@ -61,9 +63,9 @@ function buildTable(dates, openPrices, highPrices, lowPrices, closingPrices, vol
   }
 }
 
-function buildPlot(Symbol, StartDate, EndDate) {
-  var url = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&api_key=${apiKey}`;
-  console.log(url);
+function buildPlot(Symbol, StartDate, EndDate, mykeyA) {
+  var url = `https://www.quandl.com/api/v3/datasets/WIKI/${Symbol}.json?start_date=${StartDate}&end_date=${EndDate}&api_key=${mykeyA}`;
+  // console.log(url);
   d3.json(url).then(function(data) {
 
     // Grab values from the response json object to build the plots
@@ -77,7 +79,7 @@ function buildPlot(Symbol, StartDate, EndDate) {
     var lowPrices = unpack(data.dataset.data, 3);
     var closingPrices = unpack(data.dataset.data, 4);
 
-    getMonthlyData(Symbol, StartDate, EndDate);
+    getMonthlyData(Symbol, StartDate, EndDate, mykeyA);
 
     var trace1 = {
       type: "scatter",
@@ -105,17 +107,42 @@ function buildPlot(Symbol, StartDate, EndDate) {
     var layout = {
       title: `${stock} closing prices`,
       xaxis: {
+        // linecolor: ,
+        linewidth: 2,
         range: [startDate, endDate],
-        type: "date"
+        type: "date",
+        mirror: true,
       },
       yaxis: {
+        linewidth: 2,
         autorange: true,
-        type: "linear"
+        type: "linear",
+        mirror: true,
       },
-      showlegend: false
+      showlegend: true,
+      showline: true,
+      autosize: true,
+      
     };
 
-    Plotly.newPlot("plot", data, layout);
+    Plotly.newPlot("plot", data, layout, height=500);
+  //   plot.update_layout(
+  //     autosize=true,
+  //     width=500,
+  //     height=500,
+  //     margin=dict(
+  //         l=50,
+  //         r=50,
+  //         b=100,
+  //         t=100,
+  //         pad=4
+  //     ),
+  //     paper_bgcolor="LightSteelBlue",
+  // )
+  
+  // plot.show();
+  
+    
 
   });
 }
@@ -137,10 +164,14 @@ Button.on("click", () => {
   var inputField1 = $("#datepicker1").datepicker('getDate');
   $.datepicker.formatDate('dd/mm/yy', inputField1);
   var inputField2 = $("#datepicker2").datepicker('getDate');
+  // var mykeyA = $("#pass"); // .property("value").toString().trim(); 
+  // var myval = mykeyA.property("value").toString().trim();
+  // console.log(mykeyA); // .property("value").toString().trim());
   $.datepicker.formatDate('dd/mm/yy', inputField2);
   // D1=inputField1
   var formatTime = d3.timeFormat("%Y-%m-%d");
   var Symbol = inputField3.property("value").toString().toUpperCase().trim();
+  var mykeyA = mykeyAB.property("value").toString().trim();
   jj=Date.parse(inputField1);
   // console.log((inputField1));
   StartDate=formatTime(inputField1);
@@ -150,6 +181,7 @@ Button.on("click", () => {
   // console.log(inputField1);
   //console.log(inputField2);
   // console.log(jj);
-  buildPlot(Symbol, StartDate, EndDate);
+  // console.log(mykeyA);
+  buildPlot(Symbol, StartDate, EndDate, mykeyA);
 });
 buildPlot();
